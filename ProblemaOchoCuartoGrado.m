70be0a840d9835d96cd5da7741b9b301e7b37434
jennyGuadalupeAH <91712665+jennyGuadalupeAH@users.noreply.github.com>
2021-10-28 17:51:38 -0500

    ProblemaOchoCuartoGrado.m
     Determina las siguientes variables y funciones ER, grafica , obtener dominio y rango

---
+++ ProblemaOchoCuartoGrado.m
@@ -0,0 +1,24 @@
+% Scrip octave
+% Title           : Funcion real de variable real y su representacion grafica
+% Descripcion     : Determina las siguientes variables y funciones ER, grafica , obtener dominio y rango
+% Autor           : Jennifer Guadalupe Angeles Hernandez
+% Date            : 27102021
+% Version         : 1
+% Nota            : Requiere la aplicacion de Octave
+%Problema         :Ocho
+
+pkg load symbolic
+syms x
+
+%funcion de cuarto grado
+'f(x)x^4+6x^3+9x^2-1'
+% Dominio de las funciones
+x= linspace(-50,50)
+% Regla de correspondencia (funcion)
+fx=((x.^4)+(6*x.^3)+((9*x.^2)-1));
+% Plotear Funcion 
+semilogy(x,fx);
+grid on
+ylabel('y')
+xlabel('x')
+title('dominio xE R, rango yE[-1,+oo), minimos(-3,-1)(0,-1), maximo(-3/2,65/16)')
