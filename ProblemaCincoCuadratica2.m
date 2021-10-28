b45938d1a3cf68d0343fbfa8387f88d08d0b93bf
jennyGuadalupeAH <91712665+jennyGuadalupeAH@users.noreply.github.com>
2021-10-28 16:47:18 -0500

    ProblemaCincoCuadratica2.m
     Determina las siguientes variables y funciones ER, grafica , obtener dominio y rango

---
+++ ProblemaCincoCuadratica2.m
@@ -0,0 +1,25 @@
+% Scrip octave
+% Title           : Funcion real de variable real y su representacion grafica
+% Descripcion     : Determina las siguientes variables y funciones ER, grafica , obtener dominio y rango
+% Autor           : Jennifer Guadalupe Angeles Hernandez
+% Date            : 27102021
+% Version         : 1
+% Nota            : Requiere la aplicacion de Octave
+% Problema        : Cinco
+
+clear
+pkg load symbolic
+syms x
+
+%funcion cuadratica
+'f(x)=x^2+6x'
+% Dominio de las funciones
+x=[-100:10:100];
+% Regla de correspondencia (funcion)
+fx=x.^2+6*x
+% Plotear Funcion 
+plot(x,fx)
+grid on
+ylabel('y')
+xlabel('x')
+title('raices(-6,0)(0,0), dominio xE R, rango yE[-9,+oo), minimo(-3,-9)')
