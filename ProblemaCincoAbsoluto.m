02d25c7183c835f4672d0d7a25444df7ddd8ada0
jennyGuadalupeAH <91712665+jennyGuadalupeAH@users.noreply.github.com>
2021-10-28 17:07:33 -0500

    Create ProblemaCincoAbsoluto.m

---
+++ ProblemaCincoAbsoluto.m
@@ -0,0 +1,25 @@
+% Scrip octave
+% Title           : Funcion real de variable real y su representacion grafica
+% Descripcion     : Determina las siguientes variables y funciones ER, grafica , obtener dominio y rango
+% Autor           : Jennifer Guadalupe Angeles Hernandez
+% Date            : 27102021
+% Version         : 1
+% Nota            : Requiere la aplicacion de Octave
+% Problema        : Cinco
+
+clear
+pkg load symbolic
+syms x
+
+%funcion absoluto
+'g(z)=|z^3|'
+% Dominio de las funciones
+x=[-100:10:100]
+% Regla de correspondencia (funcion)
+fx=abs(x.^3);
+% Plotear Funcion 
+plot(x,fx)
+grid on
+ylabel('y')
+xlabel('x')
+title('raiz(0,0), dominio zE R, rango y E[0,+oo), minimo(0,0)')
